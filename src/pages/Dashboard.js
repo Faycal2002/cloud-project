9dbf6b48c929e1c16fb9df0f6e48ad0ddb95698e
faycal2002 <habchifaycal@yahoo.com>
2026-02-25 15:24:56 +0000

    ui login/register done

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,16 +1,18 @@
 import SideMenu from "../components/SideMenu";
+
 function Dashboard() {
     return (
-        <div className="flex">
-            <SideMenu/>
-            <div className="p-12 w-full">
-                <p className="text-lg text-gray-600 mb-8">Welcome to your personal space</p>
-                <div className="bg-white p-6 rounded-lg shadow-lg">
-                    <h3 className="text-2xl font-semibold mb-3">Statistics</h3>
-                    <p className="text-gray-600">Your statistics...</p>
+        
+            <div className="flex ">
+                <SideMenu/>
+                <div className="p-12 w-full ">
+                    <p className="text-lg text-gray-600 mb-8">Welcome to your personal space</p>
+                    <div className="bg-white p-6 rounded-lg shadow-lg">
+                        <h3 className="text-2xl font-semibold mb-3">Statistics</h3>
+                        <p className="text-gray-600">Your statistics...</p>
+                    </div>
                 </div>
             </div>
-        </div>    
     );
 }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,6 @@
 import {Link} from "react-router-dom";
 import { useState } from 'react';
+import bg from "../assets/bg.jpg"
 
 function Login() {
     const [email, setEmail] = useState('');
@@ -11,38 +12,48 @@ function Login() {
     };
 
     return (
-        <div className="p-12 max-w-6xl mx-auto">
-            <div className="max-w-md mx-auto bg-white p-8 rounded-lg shadow-lg">
-                <h1 className="text-3xl font-bold mb-6 text-center">Login</h1>
-                <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-                    <input
-                        type="email"
-                        placeholder="Email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        className="px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        required
-                    />
-                    <input
-                        type="password"
-                        placeholder="Mot de passe"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        className="px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        required
-                    />
-                    <button 
-                        type="submit"
-                        className="px-4 py-3 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition font-medium"
-                    >
-                        Sign In
-                    </button>
-                    <Link to ="/Register"  className="flex justify-center">
-                    <button type="button">Register as a client</button>
-                    </Link>
-                    <Link to = "/dashboard">
-                    <button> dashboard</button></Link>
-                </form>
+        <div
+            className="h-screen bg-cover bg-center "
+            style={{ backgroundImage: `url(${bg})` }}
+            >
+
+            
+            <div className="h-full w-full backdrop-blur-md bg-black/10 ">
+
+                <div className="p-20 max-w-6xl mx-auto">
+                    <div className="max-w-md mx-auto bg-white/50 backdrop-blur-xl border border-white/20 p-8 rounded-2xl shadow-2xl">
+                        <h1 className="text-3xl font-bold mb-6 text-center">Login</h1>
+                        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+                            <input
+                                type="email"
+                                placeholder="Email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                className="px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                required
+                            />
+                            <input
+                                type="password"
+                                placeholder="Mot de passe"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                className="px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                required
+                            />
+                            <button 
+                                type="submit"
+                                className="px-4 py-3 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition font-medium"
+                            >
+                                Sign In
+                            </button>
+                            <Link to ="/Register"  className="flex justify-center text-white">
+                            <button type="button">Register as a client</button>
+                            </Link>
+                            <Link to = "/dashboard">
+                            <button> dashboard</button></Link>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     );
