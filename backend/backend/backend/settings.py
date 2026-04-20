a4fcbf436494574dac2742936a0d3b9e7d73cfde
faycal2002 <habchifaycal@yahoo.com>
2026-04-20 17:16:52 +0100

    new

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -161,9 +161,11 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://5042982-frontend2-g6dcfjgtf2dtd3hx.uksouth-01.azurewebsites.net",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://5042982-frontend2-g6dcfjgtf2dtd3hx.uksouth-01.azurewebsites.net",
 ]
